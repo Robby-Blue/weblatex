9a7be6e447c2917aa367c530f2f9885b35478b44
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2024-10-26 22:48:45 +0200

    move files to new project system

--- src/backend/projects.py
+++ src/backend/projects.py
@@ -4,3 +4,8 @@ def add_project(creator, path, is_folder):
     db.execute(
 "INSERT INTO Projects (creator, path, is_folder) VALUES (%s, %s, %s)",
 (creator, path, is_folder))
+    
+def get_projects(creator, parent_path):
+    return db.query(
+"SELECT * FROM Projects WHERE creator=%s AND parent_path=%s",
+(creator, parent_path))
