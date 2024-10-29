18596befae4ca8fc2cbbc2eabff35a7e51ba43d8
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2024-10-29 23:03:15 +0100

    shortcuts: add toggledarkmode

--- src/frontend/editor/scripts/shortcuts.js
+++ src/frontend/editor/scripts/shortcuts.js
@@ -13,14 +13,47 @@ function table(src, pos) {
   return insertText(src, pos, text);
 }
 
+function toggledarkmode(src, pos) {
+  if(!src.includes("\\usepackage{darkmode}")){
+    let docPos = src.indexOf("documentclass")
+    let startPos = src.indexOf("\n", docPos) + 1
+    src = insertText(src, startPos, "\\usepackage{darkmode}\n")
+  }
+  if(!src.includes("\\enabledarkmode")){
+    let docPos = src.indexOf("usepackage{darkmode}")
+    let startPos = src.indexOf("\n", docPos) + 1
+    src = insertText(src, startPos, "\\enabledarkmode\n")
+    
+    return src
+  }
+
+  let commandPos = src.indexOf("\\enabledarkmode")
+  let newlinePos = src.lastIndexOf("\n", commandPos)
+  let commentPos = src.lastIndexOf("%", commandPos)
+  let commentInLine = commentPos > newlinePos
+  if(commentInLine){
+    src = removeText(src, commentPos, 1)
+  } else {
+    src = insertText(src, newlinePos+1, "%")
+  }
+  return src
+}
+
 function insertText(src, pos, text) {
   let srcStart = src.substring(0, pos);
   let srcEnd = src.substring(pos);
   return srcStart + text + srcEnd;
 }
 
+function removeText(src, pos, chars) {
+  let srcStart = src.substring(0, pos);
+  let srcEnd = src.substring(pos+chars);
+  return srcStart + srcEnd;
+}
+
 let shortcuts = {
   table: table,
+  toggledarkmode: toggledarkmode,
 };
 
 export function executeShortcut(shortcut, src, pos) {
