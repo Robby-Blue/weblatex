c098297703fdcd3d5586bc0afa6a59e9cb37b965
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2024-11-10 20:03:25 +0100

    pdf rendering: fix blurrynes

--- src/frontend/editor/scripts/pdf-viewer.js
+++ src/frontend/editor/scripts/pdf-viewer.js
@@ -1,7 +1,7 @@
 let { pdfjsLib } = globalThis;
 
 pdfjsLib.GlobalWorkerOptions.workerSrc =
-  "https://unpkg.com/pdfjs-dist@4.6.82/build/pdf.worker.min.mjs";
+  "https://unpkg.com/pdfjs-dist@4.6.82/legacy/build/pdf.worker.min.mjs";
 
 const pdfjsWorker = new pdfjsLib.PDFWorker();
 
@@ -9,18 +9,26 @@ let pathName = window.location.pathname;
 let projectPath = pathName.substring("/editor/".length);
 
 async function renderPage(page, canvas, parentWidth) {
+  // afaik force the pdf to render at 1080 pixels
+  // and upscale/downscale? it to fit that
+  let horizontalResolution = 1080;
+  let resolutionScale = horizontalResolution / parentWidth;
+
   let canvasContext = canvas.getContext("2d");
 
   let initialViewport = page.getViewport({ scale: 1 });
   let scale = parentWidth / initialViewport.width;
 
   let viewport = page.getViewport({ scale: scale });
-  canvas.width = viewport.width;
-  canvas.height = viewport.height;
+  canvas.width = viewport.width * resolutionScale;
+  canvas.height = viewport.height * resolutionScale;
+  canvas.style.width = viewport.width + "px";
+  canvas.style.height = viewport.height + "px";
 
   await page.render({
     canvasContext: canvasContext,
     viewport: viewport,
+    transform: [resolutionScale, 0, 0, resolutionScale, 0, 0],
   });
 }
 
