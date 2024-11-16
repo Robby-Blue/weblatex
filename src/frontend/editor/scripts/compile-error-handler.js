49d11e7677eeacfe3e433ade50904ba3eb5e9f43
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2024-11-16 03:17:29 +0100

    compile errors: show actual errors and lines

--- src/frontend/editor/scripts/compile-error-handler.js
+++ src/frontend/editor/scripts/compile-error-handler.js
@@ -1,6 +1,21 @@
 let popupDiv = document.getElementById("compile-error-popup");
 let titleElement = document.getElementById("compile-error-title");
+let goToLineButton = document.getElementById("compile-error-line");
+let fullErrorElement = document.getElementById("compile-error-full-error");
 let log = document.getElementById("compile-error-log");
+let codeEditorDiv = document.getElementById("editor");
+
+let lineNumber;
+
+goToLineButton.addEventListener("click", () => {
+  let scrollLine = lineNumber - 5;
+  if (scrollLine < 0) {
+    scrollLine = 0;
+  }
+  console.log(scrollLine);
+  let codeLineElement = codeEditorDiv.childNodes[scrollLine];
+  codeLineElement.scrollIntoView();
+});
 
 export function onCompileResult(data) {
   let wasSuccessful = data.return_code == 0;
@@ -18,9 +33,41 @@ export function onCompileResult(data) {
     return;
   }
 
-  titleElement.innerText = "error";
-  // parse error type here, first line
-  // starting with an exclamation mark
-  // the next lines are extra info like
-  // line number and other info
+  let parsed = parseError(data.log);
+
+  titleElement.innerText = parsed.errorMessage;
+  goToLineButton.innerText = "line " + parsed.lineNumber;
+  fullErrorElement.innerText = parsed.fullMessage;
+
+  let codeLineElement = codeEditorDiv.childNodes[parsed.lineNumber - 1];
+  codeLineElement.classList.add("highlight-error");
+
+  lineNumber = parsed.lineNumber;
+}
+
+function parseError(log) {
+  let errorMessage;
+  let fullMessage = "";
+  let lineNumber;
+
+  for (let line of log.split("\n")) {
+    if (!errorMessage) {
+      if (line.charAt(0) != "!") continue;
+      errorMessage = line.slice(1).trim();
+    } else if (errorMessage) {
+      if (line.slice(0, 2) == "l." && !lineNumber) {
+        // eg "l.1"
+        let firstWord = line.split(" ")[0];
+        lineNumber = firstWord.slice(2).trim();
+      }
+      if (line.charAt(0) == "!") break;
+      fullMessage += line + "\n";
+    }
+  }
+
+  return {
+    errorMessage,
+    lineNumber,
+    fullMessage,
+  };
 }
